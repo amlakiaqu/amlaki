3e965219fc8c239c7eb6c10e0b63c00b4ec1e3f1
amlakiaqu <amlakiaqu@gmail.com>
2017-04-23 07:17:08 +0300

    Add Js/Css files

---
+++ public/js/resources.a9b3163be2a1067978931295dd8d9085.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(document).ready(function () {
+
+    $(document).on('click', '#btn-create-post', function (e) {
+        e.preventDefault();
+        createPostCreateModal();
+    });
+
+    $(document).on('submit', "#" + Constants.CREATE_POST_FORM_ID, function (e) {
+        e.preventDefault();
+        var formData = $(this).serializeArray();
+        console.log(formData);
+    });
+});
+$(document).ready(function () {});
